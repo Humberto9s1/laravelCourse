45422228a65f72036b8e64ae747d7202a68ab02d
HUMBERTO MARTINS <humbertosant.mart@gmail.com>
2020-12-12 03:53:57 -0300

    Ligando Bootstrap ao Projeto - Aula 14

--- resources/views/produtos/index.blade.php
+++ resources/views/produtos/index.blade.php
@@ -1,16 +1,12 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Página dos produtos</h1>
-    @foreach($produtos as $produto)
+@extends('layouts.template')
+@section('title', 'Página Inicial')
+@section('content')
+<div class="container">
+<h1>Página dos Produtos</h1>
+@foreach($produtos as $produto)
     <p>{{$produto->nome}}</p>
     @endforeach
 
     {{$produtos->links()}}
-</body>
-</html>
+</div>
+@endsection
