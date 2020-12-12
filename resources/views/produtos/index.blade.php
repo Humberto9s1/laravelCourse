f9a3793493e79949dbdf84241fde1e3a40ee722a
HUMBERTO MARTINS <humbertosant.mart@gmail.com>
2020-12-12 04:19:15 -0300

    Aplicando DataTables - Aula 16

--- resources/views/produtos/index.blade.php
+++ resources/views/produtos/index.blade.php
@@ -3,10 +3,38 @@
 @section('content')
 <div class="container">
 <a href="produtos/inserir" type="button" class="mt-4 mb-4 btn btn-primary">Inserir Produtos</a>
-@foreach($produtos as $produto)
-    <p>{{$produto->nome}}</p>
-    @endforeach
 
-    {{$produtos->links()}}
+
+   <!-- DataTales Example -->
+   <div class="card shadow mb-4">
+
+<div class="card-body">
+  <div class="table-responsive">
+    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Valor</th>
+          <th>Estoque</th>
+          <th>Ações</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        @foreach($produtos as $produto)  
+        <tr>
+            <td>{{$produto->nome}}</td>
+            <td>{{$produto->valor}}</td>
+            <td>{{$produto->estoque}}</td>
+            <td></td>
+        </tr>
+        @endforeach
+      </tbody>
+  </table>
+</div>
+</div>
+</div>
+    
+    <!--{{$produtos->links()}}-->
 </div>
 @endsection
